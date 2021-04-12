b6dd63423264f4fbdc71c1788c07de7c438e6995
Matias Siro <matias.siro@outlook.com>
2021-04-12 16:00:08 +0300

    bugit korjattu. nyt ohjelman saa käyntiin, vaikka tietokantaa ei ole luotu

--- varauskalenteri/src/main/java/userinterface/MainProgram.java
+++ varauskalenteri/src/main/java/userinterface/MainProgram.java
@@ -9,10 +9,29 @@ import java.nio.file.Paths;
 
 /**
  * Tästä luokasta avataan, joko teksti- tai graafinenkäyttöliittymä.
+ * HUOM! Tietokanta luodaan toistaiseksi joka ohjelman suorittaminen aikana uudelleen.
+ * Eli vanhan tietokannan sisältö tuhotaan aina, kun ohjelma käynnistetään.
+ * Kun ajat testejä, aja ensin ohjelma ja sitten vasta testit. Näin varmistut, että
+ * testejä ajettaessa tietokanta on olmessa.
  * @author Matias Siro
  */
 public class MainProgram {
     public static void main(String[] args) {
+        try {
+            // poistetaan vahna tietokanta
+            Path dbPath = Paths.get("database.db");
+            Files.delete(dbPath);
+        } catch (Exception e) {
+            System.err.println("Poikkeus tuhottaessa tietokantaa: " + e.getMessage());
+        }
+        
+        try {
+            // luodaan uusi tietokanta
+            Controller.createDatabase();
+        } catch (Exception e) {
+            System.err.println("Poikkeus luodessa tietokantaa: " + e.getMessage());
+        }
+        
         try {
             TextBasedUserInterface.main(args);
         } catch (Exception e) {
